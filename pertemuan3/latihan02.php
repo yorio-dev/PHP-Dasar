c3afc069f217804bdc99459a3fd9c7d8c04199d0
yorio-dev <yo.rio282@gmail.com>
2020-04-16 08:24:16 +0700

    wpu

---
+++ pertemuan3/latihan02.php
@@ -0,0 +1,21 @@
+<?php 
+// Pengkondisian / Percabangan
+// if else
+// if elseif else
+// ternary
+// switch
+
+// if else
+$x = 20;
+if( $x < 20 ) {
+    echo "benar";
+} elseif( $x == 20 ) {
+    echo "Bingo!";
+} else {
+    echo "salah";
+}
+
+
+
+
+?>
